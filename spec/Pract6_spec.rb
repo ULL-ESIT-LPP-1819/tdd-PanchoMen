9681425b648cb2d5c2a410ac893a63ccb1ceba69
Francisco Mendoza <alu0100846768@ull.edu.es>
2018-11-08 18:35:45 +0000

    Creadas las pruebas para los atributos de la etiqueta

--- spec/Pract6_spec.rb
+++ spec/Pract6_spec.rb
@@ -1,9 +1,36 @@
 RSpec.describe Pract6 do
-  it "has a version number" do
-    expect(Pract6::VERSION).not_to be nil
-  end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+	#before :each do
+    	#	@etiqueta1 = Pract6.new()
+  	#end
+
+	describe "# Pruebas de existencias de los atributos #" do
+		it "Comprobar que existe un nombre de etiqueta" do
+			expect(subject).to respond_to(:nombre)
+		end
+
+		it "Comprobar que existe la cantidad de grasas" do
+                	expect(Pract6.attributes).to include(:grasas)
+        	end
+
+		it "Comprobar que existe la cantidad de grasas saturadas" do
+                	expect(Pract6.attributes).to include(:grasas_saturadas)
+        	end
+
+		it "Comprobar que existe la cantidad de hidratos de carbono" do
+                	expect(Pract6.attributes).to include(:hidratos_de_carbono)
+        	end
+
+		it "Comprobar que existe la cantidad de azúcares" do
+                	expect(Pract6.attributes).to include(:azucares)
+        	end
+
+		it "Comprobar que existe la cantidad de proteínas" do
+                	expect(Pract6.attributes).to include(:proteinas)
+        	end
+
+		it "Comprobar que existe la cantidad de sal" do
+                	expect(Pract6.attr_writer).to include(:sal)
+        	end
+	end
 end
