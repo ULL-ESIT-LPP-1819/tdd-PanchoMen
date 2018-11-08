9aeb265bf46da472bdca6eb882e8f33a05bb41c1
Francisco Mendoza <alu0100846768@ull.edu.es>
2018-11-08 17:22:32 +0000

    Creada la estructura de la practica, haciendo uso de la herramienta Bundler

---
+++ spec/Pract6_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Pract6 do
+  it "has a version number" do
+    expect(Pract6::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
