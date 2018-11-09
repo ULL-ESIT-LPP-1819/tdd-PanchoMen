933900c5a54a739ae14ff999a1ea59479e20c361
Francisco Mendoza <alu0100846768@ull.edu.es>
2018-11-09 00:42:31 +0000

    Modificadas las pruebas para los atributos de la etiqueta

--- lib/Pract6.rb
+++ lib/Pract6.rb
@@ -2,5 +2,4 @@ require "Pract6/version"
 
 module Pract6
   class Error < StandardError; end
-  # Your code goes here...
 end
--- spec/Pract6_spec.rb
+++ spec/Pract6_spec.rb
@@ -1,36 +1,36 @@
 RSpec.describe Pract6 do
 
-	#before :each do
-    	#	@etiqueta1 = Pract6.new()
-  	#end
+	before :each do
+    		@etiqueta1 = Pract6.new("Galletas", 0, 0, 0, 0, 0, 0, 0, 0)
+  	end
 
 	describe "# Pruebas de existencias de los atributos #" do
 		it "Comprobar que existe un nombre de etiqueta" do
-			expect(subject).to respond_to(:nombre)
+			expect(@etiqueta1.nombre).no_to be nil
 		end
 
 		it "Comprobar que existe la cantidad de grasas" do
-                	expect(Pract6.attributes).to include(:grasas)
+                	expect(@etiqueta1.grasas).no_to be nil
         	end
 
 		it "Comprobar que existe la cantidad de grasas saturadas" do
-                	expect(Pract6.attributes).to include(:grasas_saturadas)
+                	expect(@etiqueta1.grasas_saturadas).no_to be nil
         	end
 
 		it "Comprobar que existe la cantidad de hidratos de carbono" do
-                	expect(Pract6.attributes).to include(:hidratos_de_carbono)
+                	expect(@etiqueta1.hidratos_carbono).no_to be nil
         	end
 
 		it "Comprobar que existe la cantidad de azúcares" do
-                	expect(Pract6.attributes).to include(:azucares)
+			expect(@etiqueta1.azucares).no_to be nil
         	end
 
 		it "Comprobar que existe la cantidad de proteínas" do
-                	expect(Pract6.attributes).to include(:proteinas)
+			expect(@etiqueta1.proteinas).no_to be nil
         	end
 
 		it "Comprobar que existe la cantidad de sal" do
-                	expect(Pract6.attr_writer).to include(:sal)
+			expect(@etiqueta1.sal).no_to be nil
         	end
 	end
 end
