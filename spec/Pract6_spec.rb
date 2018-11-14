2b6ad849a0ed7e6d586fddf2cd4cdd2060a73882
Francisco Mendoza <alu0100846768@ull.edu.es>
2018-11-14 20:23:41 +0000

    Creadas las pruebas de existencias de metodos

--- spec/Pract6_spec.rb
+++ spec/Pract6_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Etiqueta do
 
 	before :each do
 		#@etiqueta1 = Pract6.new()
-		@etiqueta1 = Etiqueta.new("Galletas", 0, 0, 0, 0, 0, 0, 0)
+		@etiqueta1 = Etiqueta.new("Galletas", 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
   	end
 
 	describe "# Pruebas de existencias de los atributos #" do
@@ -35,5 +35,35 @@ RSpec.describe Etiqueta do
 		it "Comprobar que existe la cantidad de sal" do
 			expect(@etiqueta1.sal).not_to be nil
         	end
+	end
+
+	describe "# Pruebas de existencia de los metodos para acceder a los atributos #" do
+		it "Comprobar que un método para obtener el nombre" do
+			expect(@etiqueta1.nombre).to eq("Galletas")
+		end
+
+		it "Comprobar que exite un método para obtener las grasas" do
+			expect(@etiqueta1.grasas).to eq(0.0)
+                end
+
+		it "Comprobar que exite un método para obtener las grasas saturadas" do
+			expect(@etiqueta1.grasas_saturadas).to eq(0.0)
+                end
+
+		it "Comprobar que exite un método para obtener los hidratos de carbono" do
+                        expect(@etiqueta1.hidratos_carbono).to eq(0.0)
+                end
+
+		it "Comprobar que exite un método para obtener los azucares" do
+                        expect(@etiqueta1.azucares).to eq(0.0)
+                end
+
+		it "Comprobar que exite un método para obtener las proteinas" do
+                        expect(@etiqueta1.proteinas).to eq(0.0)
+                end
+
+		it "Comprobar que existe un método para obtener la sal" do
+                        expect(@etiqueta1.sal).to eq(0.0)
+                end
 	end
 end
