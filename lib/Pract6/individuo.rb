9cca694c1e80bfdcc03e1d13bf6aeaf36f0a33a2
Francisco Mendoza <alu0100846768@ull.edu.es>
2018-11-21 18:53:12 +0000

    Superada prueba

--- lib/Pract6/individuo.rb
+++ lib/Pract6/individuo.rb
@@ -1,5 +1,14 @@
 class Individuo
-	def initialize()
 
+	attr_reader :nombre, :peso, :talla, :edad, :sexo, :cir_cintura, :cir_cadera
+
+	def initialize(nombre, peso, talla, edad, sexo, cir_cintura, cir_cadera)
+		@nombre = nombre
+		@peso = peso
+                @talla = talla
+                @edad = edad
+                @sexo = sexo
+                @cir_cintura = cir_cintura
+                @cir_cadera = cir_cadera
 	end
 end
